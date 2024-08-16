88d9cce9dde42a43f7b5b1455dec936ebdb1dec7
camilitwo <camilo.igv@gmail.com>
2024-08-16 11:02:27 -0400

    Add new components for about, FAQ, footer, navbar, and welcome
    This commit introduces new standalone components: About, FAQ, Footer, Navbar, and Welcome, each with their dedicated HTML, CSS, and spec files. These components collectively enhance the structure and modularity of the application by compartmentalizing different sections of the site.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {RouterModule, RouterOutlet} from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
