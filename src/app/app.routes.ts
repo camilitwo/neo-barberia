113698bbf8661a701baac463a7366cdb7e866ffe
camilitwo <camilo.igv@gmail.com>
2024-08-16 10:54:55 -0400

    Add new components for about, FAQ, footer, navbar, and welcome
    This commit introduces new standalone components: About, FAQ, Footer, Navbar, and Welcome, each with their dedicated HTML, CSS, and spec files. These components collectively enhance the structure and modularity of the application by compartmentalizing different sections of the site.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,8 @@ import {FaqComponent} from "./faq/faq.component";
 import {AboutComponent} from "./about/about.component";
 
 export const routes: Routes = [
-  { path: 'home', component: WelcomeComponent },
+  { path: '', component: WelcomeComponent },
   { path: 'faq', component: FaqComponent },
   { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
